4628ac57e4866c1d8215f11e59a36c7f4f9e763b
noppefoxwolf <noppelabs@gmail.com>
2024-02-21 12:01:05 +0900

    Create rounded.sh

---
+++ visionOS Engineer オンラインLT会/vol5/rounded.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through all image files in the directory
+for input_image in *.png; do
+    width=$(identify -format "%w" $input_image)
+    height=$(identify -format "%h" $input_image)
+    min_side=$(($width < $height ? $width : $height))
+    round_radius=$(($min_side / 5))  # 20% of the shorter side
+
+    echo "Processing $input_image - Width: $width Height: $height Round Radius: $round_radius"
+    
+    convert $input_image \
+    \( -size ${width}x${height} xc:none -fill white -draw "roundrectangle 0,0 ${width},${height} ${round_radius},${round_radius}" \) \
+    -alpha off -compose CopyOpacity -composite $input_image
+done
